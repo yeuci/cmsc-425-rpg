1a5cc0e934e9569ea778cf8839f29396a2aa558c
yanpeter2003 <yanpeter2003@gmail.com>
2025-02-25 13:00:55 -0500

    Added CameraRotation and CharacterMove
    WASD for movement, hold right-click to rotate camera along Y-axis

---
+++ Assets/Code/CharacterMove.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+
+public class CharacterMove : MonoBehaviour
+{
+
+    public float moveSpeed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 9.8f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = controller.isGrounded;
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontal, 0, vertical);
+        if (moveDirection.magnitude > 0.1f)
+        {
+            Transform cameraTransform = Camera.main.transform;
+            Vector3 forward = cameraTransform.forward;
+            Vector3 right = cameraTransform.right;
+
+            forward.y = 0;
+            right.y = 0;
+            forward.Normalize();
+            right.Normalize();
+
+            moveDirection = forward * vertical + right * horizontal;
+            moveDirection.Normalize();
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = jumpSpeed;
+        }
+        velocity.y -= gravity * Time.deltaTime;
+        controller.Move((moveDirection * moveSpeed + velocity) * Time.deltaTime);
+    }
+}
