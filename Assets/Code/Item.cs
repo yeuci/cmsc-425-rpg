6ec9229855d05ef31b6a33d4113a1783726d2198
yeuci <nebbbnn@gmail.com>
2025-03-07 17:00:08 -0500

    item stats

--- Assets/Code/Item.cs
+++ Assets/Code/Item.cs
@@ -14,18 +14,63 @@ public enum ActionType {
     Consume
 }
 
-[CreateAssetMenu(menuName = "Scriptable object/Item")]
+public struct ItemStatistics {
+    public int attack;
+    public int defense;
+    public int health;
+    public int mana;
+    public int speed;
+
+    public ItemStatistics(int attack, int defense, int health, int mana, int speed) {
+        this.attack = attack;
+        this.defense = defense;
+        this.health = health;
+        this.mana = mana;
+        this.speed = speed;
+    }
+
+    public readonly  int Attack() {
+        return attack;
+    }
+
+    public readonly  int Defense() {
+        return defense;
+    }
+
+    public readonly  int Health() {
+        return health;
+    }
+
+    public readonly  int Mana() {
+        return mana;
+    }
+
+    public readonly  int Speed() {
+        return speed;
+    }
+
+    public readonly override string ToString() {
+        return $"Attack: {attack}, Defense: {defense}, Health: {health}, Mana: {mana}, Speed: {speed}";
+    }
+}
+
+[CreateAssetMenu(menuName = "Scriptable Item")]
 public class Item : ScriptableObject {
-    [Header("Only gameplay")]
+    [Header("Gameplay")]
 
     public ItemType type;
     public ActionType actionType;
-    public Vector2Int range = new Vector2Int(5, 4);
-
+    
+    [Header("Item Statistics")]
+    public int attack;
+    public int defense;
+    public int health;
+    public int mana;
+    public int speed;
 
     [Header("UI")]
-    public bool stackable = true;
-
-    [Header("Both")]
     public Sprite image;
+    public bool stackable = true;
+    public int maxStack;
+    public Vector2Int range = new Vector2Int(0, 5);
 }
