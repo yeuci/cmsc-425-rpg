cced74ad30cf22f6bdcf53d8636dff2162950cd6
TyrionLann1ster <altneu.jason@gmail.com>
2025-03-29 17:23:44 -0400

    Fireball Begun
    Animated a Fireball explosion, and started the process of creating.

---
+++ Assets/Code/Animations/FireballExplosion.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+public class FireballExplosion : MonoBehaviour {
+
+    int routineRun = 0;
+    Vector3 scaleFactor = new Vector3(0f,0f,0f);
+    void Start()
+    {
+        scaleFactor = transform.localScale;
+        scaleFactor*=.4f;
+        StartCoroutine(Explode());   
+    }
+
+    IEnumerator Explode() {
+        yield return new WaitForSeconds(1f);
+        while(routineRun < 10) {
+            transform.localScale+=scaleFactor;
+            routineRun++;
+            yield return new WaitForSeconds(0.01f);
+        }
+        Destroy(gameObject);
+        yield return null;
+    }
+}
