b4b93424ea787a387f7cb0411f90821a7cdca1e2
TyrionLann1ster <altneu.jason@gmail.com>
2025-04-16 13:25:37 -0400

    Update DartLaunch.cs
    Found the pressure plate, which I will use as my target.

--- Assets/Code/Animations/DartLaunch.cs
+++ Assets/Code/Animations/DartLaunch.cs
@@ -7,13 +7,15 @@ public class DartLaunch : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     //Remember that the child's parent is the dart panel
+    GameObject target;
     void Start()
     {
         //This is set up as a child of the pressure plate
         transform.localPosition = new Vector3(0,0.064000003f,-0.00487999432f);
         transform.localEulerAngles = new Vector3(-90,180,90);
         transform.localScale = new Vector3(0.25f,0.25f,4f);
-        
+        target = transform.parent.GetChild(0).gameObject;
+        Debug.Log("Target: "+target);
     }
 
     // Update is called once per frame
