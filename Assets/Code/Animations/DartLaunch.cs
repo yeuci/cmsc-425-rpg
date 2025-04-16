5f3a4cba9149cfaf6c94e80fa4a5ca4e9bf3a5e4
TyrionLann1ster <altneu.jason@gmail.com>
2025-04-16 14:03:32 -0400

    Major Updates
    - Recentered Character
    - Darts are destroyed after 5 seconds

--- Assets/Code/Animations/DartLaunch.cs
+++ Assets/Code/Animations/DartLaunch.cs
@@ -9,23 +9,34 @@ public class DartLaunch : MonoBehaviour
     //Remember that the child's parent is the dart panel
     GameObject target;
     Vector3 direction;
+    float lifeTime = 0;
     void Start()
     {
         //This is set up as a child of the pressure plate
-        transform.localPosition = new Vector3(0,0.064000003f,-0.00487999432f);
+        transform.localPosition = new Vector3(0,0.183f,-0.00487999432f);
         transform.localEulerAngles = new Vector3(-90,180,90);
         transform.localScale = new Vector3(0.25f,0.25f,4f);
         target = transform.parent.GetChild(0).gameObject;
         Debug.Log("Target: "+target);
         direction = target.transform.position - transform.position;
         direction.y = 0;
-        transform.position += direction;
+        direction *= 2;
+        //transform.position += direction;
     }
 
     // Update is called once per frame
-    IEnumerator Launch()
+    void Update()
     {
-        
-        yield return null;
+        Debug.Log("Updating");
+        lifeTime+= Time.deltaTime;
+        if(lifeTime >= 5f) {
+            Destroy(gameObject);
+        }
+        transform.position += direction*Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        Destroy(gameObject);
     }
 }
