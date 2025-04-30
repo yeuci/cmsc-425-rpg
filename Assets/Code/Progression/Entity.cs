7ea540c7eaaf3c478ee45127ce4406cb3375ec10
TyrionLann1ster <altneu.jason@gmail.com>
2025-04-30 10:20:00 -0400

    Temporary Comment
    Need to resolve getAdjustedStats

--- Assets/Code/Progression/Entity.cs
+++ Assets/Code/Progression/Entity.cs
@@ -78,14 +78,14 @@ public class Entity : MonoBehaviour
     public Stat getAdjustedStats() {
         //Attack, Defense, Health, Magic Speed
         float[] adjStats = this.stats.getStatArray();
-         foreach (ItemSave iS in equippedGear){
+        /*foreach (ItemSave iS in equippedGear){
             Item i = iS.itemData;
             adjStats[0] += i.attack;
             adjStats[1] += i.defense;
             adjStats[2] += i.health;
             adjStats[3] += i.mana;
             adjStats[4] += i.speed;
-        }
+        }*/
         Stat stats = new Stat(this.stats.level, adjStats[0],adjStats[1],adjStats[2],adjStats[3],adjStats[4], adjStats[5]);
         return stats;
     }
