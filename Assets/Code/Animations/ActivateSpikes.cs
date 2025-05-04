7751e1ece14790a3308c0fb01eddabe46f985ab4
TyrionLann1ster <altneu.jason@gmail.com>
2025-05-04 10:43:29 -0400

    Spike trap fixed
    God, I'm a fucking idiot

--- Assets/Code/Animations/ActivateSpikes.cs
+++ Assets/Code/Animations/ActivateSpikes.cs
@@ -1,18 +1,18 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
 public class ActivateSpikes : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    GameObject spikes;
     GameObject player;
     AudioSource spikeAudio;
     CharacterController controller;
+    Boolean activated = false;
     void Start()
     {
         BoxCollider hitbox = GetComponent<BoxCollider>();
         hitbox.isTrigger = true;
-        spikes = transform.parent.gameObject;
         player = GameObject.FindGameObjectWithTag("Player");
         spikeAudio = GetComponent<AudioSource>();
         controller = player.GetComponent<CharacterController>();
@@ -30,24 +30,27 @@ public class ActivateSpikes : MonoBehaviour
             Vector3 knockbackDirection = (other.transform.position - transform.position).normalized;
             StartCoroutine(Knockback(knockbackDirection));
 
-            StartCoroutine(SpikeTrapActivation());
+            if(!activated){
+                activated = true;
+                StartCoroutine(SpikeTrapActivation());
+            }
         }
     }
 
     //Trap movement should last half a second
     IEnumerator SpikeTrapActivation() {
         //The false floor is no longer a child of the spikes
-        transform.parent = null;
+        Transform spikes = transform.GetChild(0);
         int movementDirection = 1;
         spikeAudio.Play();
-        for(int i = 0; i <= 50; i++) {
+        for(int i = 0; i < 50; i++) {
             if(i == 25) {
                 movementDirection = -1;
             }
             yield return new WaitForSeconds(.01f);
-            spikes.transform.Translate(new Vector3(0f,0f,movementDirection*1.547212f/25));
+            spikes.Translate(new Vector3(0f,0f,movementDirection*1.547212f/25));
         }
-        transform.parent = spikes.transform;
+        activated = false;
         yield return null;
     }
 
